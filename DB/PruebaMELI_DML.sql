f35175b79539f577d996564d48214f57ecd3179d
cpintog28 <110699214+cpintog28@users.noreply.github.com>
2022-08-21 19:57:54 -0500

    Script DML
    Datos para las categorías y colores del producto.

---
+++ DB/PruebaMELI_DML.sql
@@ -0,0 +1,14 @@
+INSERT INTO categoria(id_categoria, categoria, descripcion) values (1,'Celulares','Smartphones');
+INSERT INTO categoria(id_categoria, categoria, descripcion) values (2,'Ropa','Ropas y accesorios');
+INSERT INTO categoria(id_categoria, categoria, descripcion) values (3,'Television','Televisión y audio');
+INSERT INTO categoria(id_categoria, categoria, descripcion) values (4,'Hogar','Hogar y muebles');
+INSERT INTO categoria(id_categoria, categoria, descripcion) values (5,'Vehiculos','Accesorios para vehículos');
+INSERT INTO categoria(id_categoria, categoria, descripcion) values (6,'Calzado','Zapatos, zapatillas');
+
+INSERT INTO color (id_color, color, descripcion) VALUES (1, 'Sin Color', 'Productos que no requieren color');
+INSERT INTO color (id_color, color, descripcion) VALUES (2, 'Blanco', NULL);
+INSERT INTO color (id_color, color, descripcion) VALUES (3, 'Azul', NULL);
+INSERT INTO color (id_color, color, descripcion) VALUES (4, 'Morado', NULL);
+INSERT INTO color (id_color, color, descripcion) VALUES (5, 'Negro', NULL);
+INSERT INTO color (id_color, color, descripcion) VALUES (6, 'Amarillo', NULL);
+INSERT INTO color (id_color, color, descripcion) VALUES (7, 'Rojo', NULL);
